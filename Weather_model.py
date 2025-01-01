45f1e21ff65e1735331db56c2dbd35e448f7bd6f
darshanm16 <darshanm1910@gmail.com>
2025-01-01 20:21:50 +0530

    commit for version 3

--- Weather_model.py
+++ Weather_model.py
@@ -1,9 +1,15 @@
-a=float(input("enter the value of a: "))
-b=float(input("enter the value of b: "))
-c=float(input("enter the value of c: "))
-x=int(input("enter the value of x: "))
-y=a*(x**2)+b*x+c
-print("\nVersion 2\n")
-print("Quadratic Weather Model")
-print(f"Equation: y={a}x^2+{b}x={c}")
-print(f"Prediction for x={x}: y={y}\n")
+def weathering_model():
+    with open('input1.txt', 'r') as file:
+        lines = file.readlines()
+        a = float(lines[0])
+        b = float(lines[1])
+        c = float(lines[2])
+        x = float(lines[3])
+
+    y = a * x**2 + b * x + c
+    print("\nVersion 3\n")
+    print("Quadratic Weather Model")
+    print(f"Equation: y={a}x^2+{b}x={c}")
+    print(f"Prediction for x={x}: y={y}\n")
+
+weathering_model()
