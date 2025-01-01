0501da91cd8b7094213388e78e7e2379287da7f6
darshanm16 <darshanm1910@gmail.com>
2025-01-01 19:31:23 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Weather modeling Experiment 01
---
+++ Weather_model.py
@@ -0,0 +1,9 @@
+a=12
+b=5
+c=8
+x=3
+y=a*(x**2)+b*x+c
+print("\nVersion 1\n")
+print("Quadratic Weather Model")
+print(f"Equation: y={a}x^2+{b}x={c}")
+print(f"Prediction for x={x}: y={y}\n")
