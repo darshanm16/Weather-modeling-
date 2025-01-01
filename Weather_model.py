96ccea8e8f9c9db6127bd38c982494c7155cb6cb
darshanm16 <darshanm1910@gmail.com>
2025-01-01 20:13:49 +0530

    commit for version 2

--- Weather_model.py
+++ Weather_model.py
@@ -1,9 +1,9 @@
-a=12
-b=5
-c=8
-x=3
+a=float(input("enter the value of a: "))
+b=float(input("enter the value of b: "))
+c=float(input("enter the value of c: "))
+x=int(input("enter the value of x: "))
 y=a*(x**2)+b*x+c
-print("\nVersion 1\n")
+print("\nVersion 2\n")
 print("Quadratic Weather Model")
 print(f"Equation: y={a}x^2+{b}x={c}")
 print(f"Prediction for x={x}: y={y}\n")
