e3e68adea75b6beb23eccc51e0312ba0901b776e
darshanm16 <darshanm1910@gmail.com>
2025-01-02 12:05:54 +0530

    fixed some errors

--- Weather_model.py
+++ Weather_model.py
@@ -1,19 +1,3 @@
-# def weathering_model():
-#     with open('input1.txt', 'r') as file:
-#         lines = file.readlines()
-#         a = float(lines[0])
-#         b = float(lines[1])
-#         c = float(lines[2])
-#         x = float(lines[3])
-
-#     y = a * x**2 + b * x + c
-#     print("\nVersion 3\n")
-#     print("Quadratic Weather Model")
-#     print(f"Equation: y={a}x^2+{b}x={c}")
-#     print(f"Prediction for x={x}: y={y}\n")
-
-# weathering_model()
-
 import matplotlib.pyplot as plt
 
 y_val = []
